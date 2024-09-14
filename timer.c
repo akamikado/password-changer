608b0841f01c89fac8f827e0a5741121570ba7e7
akamikado <manishvasireddy1234@gmail.com>
2024-09-14 20:43:58 +0530

    feat: created timer file

--- timer.c
+++ timer.c
@@ -1,2 +1,10 @@
+#include "timer.h"
 #include <stdio.h>
 #include <unistd.h>
+
+void backout(int n) {
+  for (int i = n; i > 0; i--) {
+    printf("Wait for %d seconds\n", i);
+    sleep(1);
+  }
+}
